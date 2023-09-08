c7e3a7d1650bc103919b7befd1d5fd69c99baa8a
Angel Velasquez <pcisavel@upc.edu.pe>
2023-09-08 15:38:20 -0500

    feat(article): added article entity.

---
+++ src/app/news/model/article.entity.ts
@@ -0,0 +1,16 @@
+import {Source} from "./source.entity";
+
+export class Article {
+  title: string;
+  description: string;
+  url: string;
+  urlToImage: string;
+  source: Source;
+  constructor() {
+    this.title = '';
+    this.description = '';
+    this.url = '';
+    this.urlToImage = '';
+    this.source = new Source();
+  }
+}
