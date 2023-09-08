5a9ff2c296e96125251a4e065aa408703d9f0ad2
Angel Velasquez <pcisavel@upc.edu.pe>
2023-09-08 15:39:28 -0500

    feat(main-content): scoped articles array to article type.

--- src/app/news/components/main-content/main-content.component.ts
+++ src/app/news/components/main-content/main-content.component.ts
@@ -1,4 +1,5 @@
 import {Component, Input} from '@angular/core';
+import {Article} from "../../model/article.entity";
 
 @Component({
   selector: 'app-main-content',
@@ -7,6 +8,6 @@ import {Component, Input} from '@angular/core';
 })
 export class MainContentComponent {
   title = 'catch-up';
-  @Input() articles: Array<any> = [];
+  @Input() articles: Array<Article> = [];
 
 }
